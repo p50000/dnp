6a693842983016cb8e1ea2bd40bdba0bfb36a808
p50000 <rentacat45@gmail.com>
2022-10-12 22:15:18 +0300

    fucking shit i added one (1) function

--- chord_pb2.py
+++ chord_pb2.py
@@ -13,7 +13,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0b\x63hord.proto\":\n\x10TSuccessResponse\x12\x15\n\ris_successful\x18\x01 \x01(\x08\x12\x0f\n\x07message\x18\x02 \x01(\t\"0\n\x10TRegisterRequest\x12\x0e\n\x06ipaddr\x18\x01 \x01(\t\x12\x0c\n\x04port\x18\x02 \x01(\x05\"0\n\x11TRegisterResponse\x12\n\n\x02id\x18\x01 \x01(\x05\x12\x0f\n\x07message\x18\x02 \x01(\t\" \n\x12TDeregisterRequest\x12\n\n\x02id\x18\x01 \x01(\x05\"*\n\nTIpAndPort\x12\x0e\n\x06ipaddr\x18\x01 \x01(\t\x12\x0c\n\x04port\x18\x02 \x01(\x05\")\n\x1bTPopulateFingerTableRequest\x12\n\n\x02id\x18\x01 \x01(\x05\":\n\x1cTPopulateFingerTableResponse\x12\x1a\n\x05nodes\x18\x01 \x03(\x0b\x32\x0b.TIpAndPort\"\x16\n\x14TGetChordInfoRequest\"3\n\x15TGetChordInfoResponse\x12\x1a\n\x05nodes\x18\x01 \x03(\x0b\x32\x0b.TIpAndPort\"\x18\n\x16TGetFingerTableRequest\"5\n\x17TGetFingerTableResponse\x12\x1a\n\x05nodes\x18\x01 \x03(\x0b\x32\x0b.TIpAndPort\")\n\x0cTSaveRequest\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\x0c\n\x04text\x18\x02 \x01(\t\"\x1a\n\x0bTKeyRequest\x12\x0b\n\x03key\x18\x01 \x01(\t2\x8a\x02\n\x08Registry\x12\x31\n\x08register\x12\x11.TRegisterRequest\x1a\x12.TRegisterResponse\x12\x34\n\nderegister\x12\x13.TDeregisterRequest\x1a\x11.TSuccessResponse\x12T\n\x15populate_finger_table\x12\x1c.TPopulateFingerTableRequest\x1a\x1d.TPopulateFingerTableResponse\x12?\n\x0eget_chord_info\x12\x15.TGetChordInfoRequest\x1a\x16.TGetChordInfoResponse2\xc3\x01\n\x04Node\x12=\n\x08register\x12\x17.TGetFingerTableRequest\x1a\x18.TGetFingerTableResponse\x12(\n\x04save\x12\r.TSaveRequest\x1a\x11.TSuccessResponse\x12\'\n\x04\x66ind\x12\x0c.TKeyRequest\x1a\x11.TSuccessResponse\x12)\n\x06remove\x12\x0c.TKeyRequest\x1a\x11.TSuccessResponseb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x0b\x63hord.proto\":\n\x10TSuccessResponse\x12\x15\n\ris_successful\x18\x01 \x01(\x08\x12\x0f\n\x07message\x18\x02 \x01(\t\"\x08\n\x06TEmpty\"0\n\x10TRegisterRequest\x12\x0e\n\x06ipaddr\x18\x01 \x01(\t\x12\x0c\n\x04port\x18\x02 \x01(\x05\"0\n\x11TRegisterResponse\x12\n\n\x02id\x18\x01 \x01(\x05\x12\x0f\n\x07message\x18\x02 \x01(\t\" \n\x12TDeregisterRequest\x12\n\n\x02id\x18\x01 \x01(\x05\"*\n\nTIpAndPort\x12\x0e\n\x06ipaddr\x18\x01 \x01(\t\x12\x0c\n\x04port\x18\x02 \x01(\x05\")\n\x1bTPopulateFingerTableRequest\x12\n\n\x02id\x18\x01 \x01(\x05\":\n\x1cTPopulateFingerTableResponse\x12\x1a\n\x05nodes\x18\x01 \x03(\x0b\x32\x0b.TIpAndPort\"\x16\n\x14TGetChordInfoRequest\"3\n\x15TGetChordInfoResponse\x12\x1a\n\x05nodes\x18\x01 \x03(\x0b\x32\x0b.TIpAndPort\"\x18\n\x16TGetFingerTableRequest\"5\n\x17TGetFingerTableResponse\x12\x1a\n\x05nodes\x18\x01 \x03(\x0b\x32\x0b.TIpAndPort\")\n\x0cTSaveRequest\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\x0c\n\x04text\x18\x02 \x01(\t\"\x1a\n\x0bTKeyRequest\x12\x0b\n\x03key\x18\x01 \x01(\t2\xb6\x02\n\x08Registry\x12*\n\x0cservice_info\x12\x07.TEmpty\x1a\x11.TSuccessResponse\x12\x31\n\x08register\x12\x11.TRegisterRequest\x1a\x12.TRegisterResponse\x12\x34\n\nderegister\x12\x13.TDeregisterRequest\x1a\x11.TSuccessResponse\x12T\n\x15populate_finger_table\x12\x1c.TPopulateFingerTableRequest\x1a\x1d.TPopulateFingerTableResponse\x12?\n\x0eget_chord_info\x12\x15.TGetChordInfoRequest\x1a\x16.TGetChordInfoResponse2\xef\x01\n\x04Node\x12*\n\x0cservice_info\x12\x07.TEmpty\x1a\x11.TSuccessResponse\x12=\n\x08register\x12\x17.TGetFingerTableRequest\x1a\x18.TGetFingerTableResponse\x12(\n\x04save\x12\r.TSaveRequest\x1a\x11.TSuccessResponse\x12\'\n\x04\x66ind\x12\x0c.TKeyRequest\x1a\x11.TSuccessResponse\x12)\n\x06remove\x12\x0c.TKeyRequest\x1a\x11.TSuccessResponseb\x06proto3')
 
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'chord_pb2', globals())
@@ -22,32 +22,34 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   DESCRIPTOR._options = None
   _TSUCCESSRESPONSE._serialized_start=15
   _TSUCCESSRESPONSE._serialized_end=73
-  _TREGISTERREQUEST._serialized_start=75
-  _TREGISTERREQUEST._serialized_end=123
-  _TREGISTERRESPONSE._serialized_start=125
-  _TREGISTERRESPONSE._serialized_end=173
-  _TDEREGISTERREQUEST._serialized_start=175
-  _TDEREGISTERREQUEST._serialized_end=207
-  _TIPANDPORT._serialized_start=209
-  _TIPANDPORT._serialized_end=251
-  _TPOPULATEFINGERTABLEREQUEST._serialized_start=253
-  _TPOPULATEFINGERTABLEREQUEST._serialized_end=294
-  _TPOPULATEFINGERTABLERESPONSE._serialized_start=296
-  _TPOPULATEFINGERTABLERESPONSE._serialized_end=354
-  _TGETCHORDINFOREQUEST._serialized_start=356
-  _TGETCHORDINFOREQUEST._serialized_end=378
-  _TGETCHORDINFORESPONSE._serialized_start=380
-  _TGETCHORDINFORESPONSE._serialized_end=431
-  _TGETFINGERTABLEREQUEST._serialized_start=433
-  _TGETFINGERTABLEREQUEST._serialized_end=457
-  _TGETFINGERTABLERESPONSE._serialized_start=459
-  _TGETFINGERTABLERESPONSE._serialized_end=512
-  _TSAVEREQUEST._serialized_start=514
-  _TSAVEREQUEST._serialized_end=555
-  _TKEYREQUEST._serialized_start=557
-  _TKEYREQUEST._serialized_end=583
-  _REGISTRY._serialized_start=586
-  _REGISTRY._serialized_end=852
-  _NODE._serialized_start=855
-  _NODE._serialized_end=1050
+  _TEMPTY._serialized_start=75
+  _TEMPTY._serialized_end=83
+  _TREGISTERREQUEST._serialized_start=85
+  _TREGISTERREQUEST._serialized_end=133
+  _TREGISTERRESPONSE._serialized_start=135
+  _TREGISTERRESPONSE._serialized_end=183
+  _TDEREGISTERREQUEST._serialized_start=185
+  _TDEREGISTERREQUEST._serialized_end=217
+  _TIPANDPORT._serialized_start=219
+  _TIPANDPORT._serialized_end=261
+  _TPOPULATEFINGERTABLEREQUEST._serialized_start=263
+  _TPOPULATEFINGERTABLEREQUEST._serialized_end=304
+  _TPOPULATEFINGERTABLERESPONSE._serialized_start=306
+  _TPOPULATEFINGERTABLERESPONSE._serialized_end=364
+  _TGETCHORDINFOREQUEST._serialized_start=366
+  _TGETCHORDINFOREQUEST._serialized_end=388
+  _TGETCHORDINFORESPONSE._serialized_start=390
+  _TGETCHORDINFORESPONSE._serialized_end=441
+  _TGETFINGERTABLEREQUEST._serialized_start=443
+  _TGETFINGERTABLEREQUEST._serialized_end=467
+  _TGETFINGERTABLERESPONSE._serialized_start=469
+  _TGETFINGERTABLERESPONSE._serialized_end=522
+  _TSAVEREQUEST._serialized_start=524
+  _TSAVEREQUEST._serialized_end=565
+  _TKEYREQUEST._serialized_start=567
+  _TKEYREQUEST._serialized_end=593
+  _REGISTRY._serialized_start=596
+  _REGISTRY._serialized_end=906
+  _NODE._serialized_start=909
+  _NODE._serialized_end=1148
 # @@protoc_insertion_point(module_scope)
--- chord_pb2_grpc.py
+++ chord_pb2_grpc.py
@@ -14,6 +14,11 @@ class RegistryStub(object):
         Args:
             channel: A grpc.Channel.
         """
+        self.service_info = channel.unary_unary(
+                '/Registry/service_info',
+                request_serializer=chord__pb2.TEmpty.SerializeToString,
+                response_deserializer=chord__pb2.TSuccessResponse.FromString,
+                )
         self.register = channel.unary_unary(
                 '/Registry/register',
                 request_serializer=chord__pb2.TRegisterRequest.SerializeToString,
@@ -38,6 +43,12 @@ class RegistryStub(object):
 
 class RegistryServicer(object):
     """Missing associated documentation comment in .proto file."""
+
+    def service_info(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
 
     def register(self, request, context):
         """Missing associated documentation comment in .proto file."""
@@ -66,6 +77,11 @@ class RegistryServicer(object):
 
 def add_RegistryServicer_to_server(servicer, server):
     rpc_method_handlers = {
+            'service_info': grpc.unary_unary_rpc_method_handler(
+                    servicer.service_info,
+                    request_deserializer=chord__pb2.TEmpty.FromString,
+                    response_serializer=chord__pb2.TSuccessResponse.SerializeToString,
+            ),
             'register': grpc.unary_unary_rpc_method_handler(
                     servicer.register,
                     request_deserializer=chord__pb2.TRegisterRequest.FromString,
@@ -95,6 +111,23 @@ def add_RegistryServicer_to_server(servicer, server):
  # This class is part of an EXPERIMENTAL API.
 class Registry(object):
     """Missing associated documentation comment in .proto file."""
+
+    @staticmethod
+    def service_info(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(request, target, '/Registry/service_info',
+            chord__pb2.TEmpty.SerializeToString,
+            chord__pb2.TSuccessResponse.FromString,
+            options, channel_credentials,
+            insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
 
     @staticmethod
     def register(request,
@@ -174,6 +207,11 @@ class NodeStub(object):
         Args:
             channel: A grpc.Channel.
         """
+        self.service_info = channel.unary_unary(
+                '/Node/service_info',
+                request_serializer=chord__pb2.TEmpty.SerializeToString,
+                response_deserializer=chord__pb2.TSuccessResponse.FromString,
+                )
         self.register = channel.unary_unary(
                 '/Node/register',
                 request_serializer=chord__pb2.TGetFingerTableRequest.SerializeToString,
@@ -198,6 +236,12 @@ class NodeStub(object):
 
 class NodeServicer(object):
     """Missing associated documentation comment in .proto file."""
+
+    def service_info(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
 
     def register(self, request, context):
         """Missing associated documentation comment in .proto file."""
@@ -226,6 +270,11 @@ class NodeServicer(object):
 
 def add_NodeServicer_to_server(servicer, server):
     rpc_method_handlers = {
+            'service_info': grpc.unary_unary_rpc_method_handler(
+                    servicer.service_info,
+                    request_deserializer=chord__pb2.TEmpty.FromString,
+                    response_serializer=chord__pb2.TSuccessResponse.SerializeToString,
+            ),
             'register': grpc.unary_unary_rpc_method_handler(
                     servicer.register,
                     request_deserializer=chord__pb2.TGetFingerTableRequest.FromString,
@@ -255,6 +304,23 @@ def add_NodeServicer_to_server(servicer, server):
  # This class is part of an EXPERIMENTAL API.
 class Node(object):
     """Missing associated documentation comment in .proto file."""
+
+    @staticmethod
+    def service_info(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(request, target, '/Node/service_info',
+            chord__pb2.TEmpty.SerializeToString,
+            chord__pb2.TSuccessResponse.FromString,
+            options, channel_credentials,
+            insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
 
     @staticmethod
     def register(request,
